0f4b2cf0a6cab810fa7c1c3a8ec63ad290d8ca4f
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-11-16 09:01:31 +0000

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_16_075853) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "cart_items", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "customer_id", null: false
-    t.integer "amount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
-    t.index ["item_id"], name: "index_cart_items_on_item_id"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "items", force: :cascade do |t|
-    t.integer "genre_id", null: false
-    t.string "name", null: false
-    t.string "description", null: false
-    t.integer "price", null: false
-    t.boolean "is_active", default: true, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["genre_id"], name: "index_items_on_genre_id"
-  end
-
-  create_table "order_details", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.integer "item_id", null: false
-    t.integer "amount", null: false
-    t.integer "price", null: false
-    t.integer "making_status", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_order_details_on_item_id"
-    t.index ["order_id"], name: "index_order_details_on_order_id"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.integer "customer_id", null: false
-    t.integer "payment_method", default: 0, null: false
-    t.string "post_code", null: false
-    t.string "address", null: false
-    t.string "name", null: false
-    t.integer "shipping_fee", null: false
-    t.integer "total_payment", null: false
-    t.integer "status", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id"], name: "index_orders_on_customer_id"
-  end
-
-  add_foreign_key "cart_items", "customers"
-  add_foreign_key "cart_items", "items"
-  add_foreign_key "items", "genres"
-  add_foreign_key "order_details", "items"
-  add_foreign_key "order_details", "orders"
-  add_foreign_key "orders", "customers"
 end
