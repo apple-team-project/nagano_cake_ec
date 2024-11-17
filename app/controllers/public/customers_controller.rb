7d1ae337c7b50bb7b1f552a49be50a8a5e7d48f2
Phone Haraguchi’s <hr48691226@gmail.com>
2024-11-17 18:47:18 +0900

    public/会員情報編集機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,19 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to mypage_customers_path
+    else
+      render :edit
+    end
   end
 
   def confirm_leave
@@ -13,4 +21,11 @@ class Public::CustomersController < ApplicationController
 
   def leave
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email)
+  end
+
 end
