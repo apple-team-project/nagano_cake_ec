14821785edb6555b463c6db64c45070dc1d3371e
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-11-17 09:58:32 +0000

    admin_items_controller_show/edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,21 @@
 class Admin::ItemsController < ApplicationController
+
+  def show
+    @item=Item.find(params[:id])
+  end 
+  
+  def edit
+    @item=Item.find(params:id)
+  end 
+  
+  def update
+    item=Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item)
+  end   
+
+  private
+   def item_params
+    params.require(:item).permit(:item_image, :name, :description, :genre_id, :price, :is_active)
+   end  
 end
