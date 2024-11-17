4dbad123d8d6ed5714b1740126bc466d8ab4f6db
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-11-17 07:30:53 +0000

    customer_sign_in/up/out

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -19,11 +19,13 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  def after_path_for(resource)
+  def after_sign_in_path_for(resource)
     items_path
   end 
   
-  
+  def after_sign_out_path_for(resource)
+    "/"
+  end  
 
   # protected
 
