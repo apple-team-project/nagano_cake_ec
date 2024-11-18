d1a520b099a29155233311b9d9a41680be74fa8c
Phone Haraguchi’s <hr48691226@gmail.com>
2024-11-18 16:15:36 +0900

    注文履歴一覧、注文履歴詳細機能と画面の作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,9 +6,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = current_customer.orders.includes(order_details: :items)
   end
 
   def show
+    @order = current_customer.orders.find(params[:id])
+    @order_details = @order.order_details.includes(:item)
   end
 
   def confirm_order
