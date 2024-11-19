76df21f69e2331e809dd1b683c073b8644eb2ceb
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-11-19 05:33:01 +0000

    customer_top_about

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,7 @@ class Admin::ItemsController < ApplicationController
   end   
 
   def index
-    @items=Item.includes(:genre).order(created_at: :desc).page(params[:page]).per(10)
+    @items=Item.includes(:genre).order(created_at: :asc).page(params[:page]).per(10)
   end   
 
 
