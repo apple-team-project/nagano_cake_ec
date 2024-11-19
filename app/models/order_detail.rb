ebc6e62792a4f6e1341e070e812eb3dc965b83d9
Tatsuya Miyagawa <zgmf326@gamil.com>
2024-11-19 14:53:11 +0900

    admin_homes

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,6 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :order
+  belongs_to :item
+  
   enum making_status: { start_not_possible: 0, make_pending: 1, making: 2, make_complete: 3 }
 end
