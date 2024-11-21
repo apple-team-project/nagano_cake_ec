4d8d1662f039579a1db2e81957f674ce82e7e981
しろくん <minori@sotokanmikinonotobukkukonpyuta.local>
2024-11-21 15:15:41 +0900

    admin_order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,10 +9,12 @@ class Admin::OrdersController < ApplicationController
  
     if @order.update(order_params)
       flash.now[:notice] = '注文ステータスを更新しました。'
+      redirect_to request.referer
     else
       flash.now[:alert] = '更新に失敗しました。'
     end
-      redirect_to admin_order_path(@order)
+      
+      
   end
 
  private
