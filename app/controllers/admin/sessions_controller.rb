5906ae8b07180500d54dca71504cce1a12de0d05
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-11-22 22:58:51 +0900

    リンク・validates修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -19,7 +19,9 @@ class Admin::SessionsController < Devise::SessionsController
    #end
 
   # protected
-
+    def after_sign_in_path_for(resource)
+      admin_root_path
+    end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
