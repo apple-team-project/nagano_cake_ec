b535092475164d54a1d7713d33e58f87a4955ade
kimikuma <182318858+kimikuma@users.noreply.github.com>
2024-11-23 12:18:13 +0900

    再修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,5 @@
 class Public::OrdersController < ApplicationController
+  before_action :ensure_cart_has_items, only: [:new, :create]
   def new
     @order = current_customer.orders.build
   end
@@ -59,5 +60,11 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:payment_method, :post_code, :address, :name, :shipping_fee, :total_payment)
+  end
+
+  def ensure_cart_has_items
+    if current_customer.cart_items.empty?
+      redirect_to cart_items_path
+    end
   end
 end
